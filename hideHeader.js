78cde7f2fb9bdc33cd2fcc4894f4eddb8dd5973c
johannes <johannes@pej.se>
2021-05-23 14:25:54 +0200

    Make navigation sidebar fill to the top when the header hides

--- hideHeader.js
+++ hideHeader.js
@@ -1,6 +1,7 @@
 const timeDelta = 1000;
 var scrollPositionTimeDeltaAgo = 0;
 const header = document.getElementsByTagName('devsite-header')[0];
+const nav = document.getElementsByTagName('devsite-book-nav')[0];
 const height = header.scrollHeight;
 
 
@@ -8,15 +9,21 @@ document.addEventListener('scroll', (event) => {
     const scrollY = window.scrollY;
     const delta = scrollY - scrollPositionTimeDeltaAgo;
 
-    if (!header) return;
+    if (!header || !nav) return;
 
     setTimeout(() => scrollPositionTimeDeltaAgo = scrollY, timeDelta);
 
     if (scrollY > height && delta + height > 0) {
+        nav.style['max-height'] = '100%';
         header.style.transition = 'transform 0.5s ease-in 0s';
         header.style.transform = `translateY(-${height}px)`;
+        nav.style.transition = 'transform 0.5s ease-in 0s';
+        nav.style.transform = `translateY(-${height}px)`;
     } else {
+        nav.style['max-height'] = `calc(100% - ${height}px)`;
         header.style.transition = 'transform 0.5s ease-out 0s';
         header.style.transform = 'translateY(0px)';
+        nav.style.transition = 'transform 0.5s ease-out 0s';
+        nav.style.transform = 'translateY(0px)';
     }
 });
