11427a391c734f53650b299fe90d9287b56b1a39
johannes <johannes@pej.se>
2021-05-23 14:40:22 +0200

    feat: variable hide time

--- hideHeader.js
+++ hideHeader.js
@@ -1,4 +1,10 @@
+// How fast the header hides / unhided
+const hideTime = '0.3s';
+
+// When scrolling up the header unhides if scrolling
+//  `height of the header` in `timeDelta` ms
 const timeDelta = 1000;
+
 var scrollPositionTimeDeltaAgo = 0;
 const header = document.getElementsByTagName('devsite-header')[0];
 const nav = document.getElementsByTagName('devsite-book-nav')[0];
@@ -15,15 +21,15 @@ document.addEventListener('scroll', (event) => {
 
     if (scrollY > height && delta + height > 0) {
         nav.style['max-height'] = '100%';
-        header.style.transition = 'transform 0.5s ease-in 0s';
+        header.style.transition = `transform ${hideTime} ease-in 0s`;
         header.style.transform = `translateY(-${height}px)`;
-        nav.style.transition = 'transform 0.5s ease-in 0s';
+        nav.style.transition = `transform ${hideTime} ease-in 0s`;
         nav.style.transform = `translateY(-${height}px)`;
     } else {
         nav.style['max-height'] = `calc(100% - ${height}px)`;
-        header.style.transition = 'transform 0.5s ease-out 0s';
+        header.style.transition = `transform ${hideTime} ease-out 0s`;
         header.style.transform = 'translateY(0px)';
-        nav.style.transition = 'transform 0.5s ease-out 0s';
+        nav.style.transition = `transform ${hideTime} ease-out 0s`;
         nav.style.transform = 'translateY(0px)';
     }
 });
